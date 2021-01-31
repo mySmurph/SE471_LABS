d93de9ebdf2a72128ff869ab6bd9a053ee2e5355
mySmurph <56486787+mySmurph@users.noreply.github.com>
2021-01-31 12:44:11 -0800

    Part C done

--- LineDrawing/LineDrawingTest.java
+++ LineDrawing/LineDrawingTest.java
@@ -4,15 +4,15 @@ import javax.swing.JFrame;
 
 public class LineDrawingTest {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        JFrame application = new JFrame();
-        LiningPanel panel = new LiningPanel();
+		JFrame application = new JFrame();
+		LiningPanel panel = new LiningPanel();
 
-        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        application.add(panel);
-        application.setSize(300, 300);
-        application.setTitle("Lining Art");
-        application.setVisible(true);
-    }
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(300, 300);
+		application.setTitle("Lining Art");
+		application.setVisible(true);
+	}
 }
--- LineDrawing/LiningPanel.java
+++ LineDrawing/LiningPanel.java
@@ -5,24 +5,28 @@ import java.awt.Graphics;
 
 public class LiningPanel extends javax.swing.JPanel {
 
-    public LiningPanel() { }
+	public LiningPanel() { }
 
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-        int x1 = getWidth();
-        int y1 = getHeight();
-        
-        double lines = 15.0;
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		int w = getWidth();
+		int h = getHeight();
+		
+		double lines = 15.0;
 
-        for(int i = 0; i < lines; i++)
-        {
-            int x2 = (int)((i/lines)*x1); 
-            int y2 = (int)((i/lines)*y1); 
-            
-            g.drawLine(0,  y2, x2, y1);
-        }
-        
-    }
-    
+		for(int i = 0; i < lines; i++)
+		{
+			int w2 = (int)((i/lines)*w); 
+			int h2 = (int)((i/lines)*h); 
+			
+		//	g.drawLine(x,  y, x1, y2);
+			g.drawLine(0,  h2, w2, h);
+			g.drawLine(w2,  h, w, h-h2);
+			g.drawLine(w,  h-h2, w-w2, 0);
+			g.drawLine(w-w2, 0, 0, h2);
+		}
+		
+	}
+	
 }
