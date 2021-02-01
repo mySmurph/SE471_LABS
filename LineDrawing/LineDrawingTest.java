f5b6fcddcf07af6c9a542626d2d777ed6a7f00d7
mySmurph <56486787+mySmurph@users.noreply.github.com>
2021-01-31 16:16:11 -0800

    Inserted button and changed Jframe

--- LineDrawing/LineDrawingTest.java
+++ LineDrawing/LineDrawingTest.java
@@ -1,18 +1,60 @@
 package LineDrawing;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LineDrawingTest {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class LineDrawingTest extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;
+
+	private JButton playButton;
+	private boolean isPlaying;
+	private Thread rainbow;
+
+	public LineDrawingTest() {
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel buttonPanel = new JPanel();
+		playButton = new JButton("Start");
+		playButton.addActionListener(this);
+		buttonPanel.add(playButton);
+		getContentPane().add(buttonPanel, BorderLayout.NORTH);
+
+		Runnable panel = new LiningPanel();
+		rainbow = new Thread(panel);
+
+		getContentPane().add(((LiningPanel) panel).getPanel(), BorderLayout.CENTER);
+		this.setSize(300, 350);
+		this.setTitle("Lining Art");
+		this.setVisible(true);
+	}
+		/**
+	 * Shuffles  and repaints the panels with new cards
+	 * @param    e    event handler
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(isPlaying){
+			//stop
+			playButton.setText("Start");
+		}else{
+			//go
+			playButton.setText("Stop");
+		}
+		isPlaying = !isPlaying; 
+	}
+
 
 	public static void main(String[] args) {
+		new LineDrawingTest();
 
-		JFrame application = new JFrame();
-		LiningPanel panel = new LiningPanel();
-
-		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		application.add(panel);
-		application.setSize(300, 300);
-		application.setTitle("Lining Art");
-		application.setVisible(true);
 	}
+
 }
--- LineDrawing/LiningPanel.java
+++ LineDrawing/LiningPanel.java
@@ -4,10 +4,31 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-public class LiningPanel extends javax.swing.JPanel {
+import javax.swing.JPanel;
 
-	public LiningPanel() { }
+public class LiningPanel extends javax.swing.JPanel implements Runnable{
 
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LiningPanel() {
+	}
+
+	@Override
+	public void run(){
+		try {
+			Thread.sleep(0);
+			this.repaint();
+		} catch (InterruptedException ex) {
+			System.out.println("I'm interrupted");
+		}
+	}
+
+	public LiningPanel getPanel(){
+		return this;
+	}
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
@@ -23,13 +44,13 @@ public class LiningPanel extends javax.swing.JPanel {
 			int w2 = (int)((i/lines)*w); 
 			int h2 = (int)((i/lines)*h); 
 			
-			//d.	Add random colors to the lines 
+		//d.	Add random colors to the lines 
 			int red = random.nextInt(255);
 			int green = random.nextInt(255);
 			int blue = random.nextInt(255);
 			g.setColor(new Color(red, green, blue));
 
-			// c.	Modify the code to mirror the design in all four corners, as shown in the figure below	
+		// c.	Modify the code to mirror the design in all four corners, as shown in the figure below	
 		//	g.drawLine(x1, y1, x2, y2) <-- order of lines
 			g.drawLine(0,  h2, w2, h);
 			g.drawLine(w2,  h, w, h-h2);
