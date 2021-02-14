bc84ae83ad9845a01f4a24108a213e33a5cd66cf
laurengonz <laurengonz@users.noreply.github.com>
2021-02-14 13:31:38 -0800

    sorting utility log + bubble

--- 2_SortingUtility/src/SortingUtility.java
+++ 2_SortingUtility/src/SortingUtility.java
@@ -8,13 +8,13 @@ public class SortingUtility {
 	
 	public ArrayList<Product> sort(ArrayList<Product> items, int sortingApproach){
 		switch(sortingApproach) {
-		case 1:		return bubleSort(items);
+		case 1:		return bubbleSort(items);
 		case 2: 	return quickSort(items);
 		}
 		return items;
 	}
 	
-	private ArrayList<Product> bubleSort(ArrayList<Product>  items){
+	private ArrayList<Product> bubbleSort(ArrayList<Product>  items){
 		return items;
 	}
 	
---
+++ 2_SortingUtility/src/SortingUtilityLog.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.ArrayList;
+
+public class SortingUtilityLog extends SortingUtility {
+
+	public SortingUtilityLog() {}
+	
+	private ArrayList<Product> bubbleSort(ArrayList<Product> items){
+		return items;
+	}
+		
+	private ArrayList<Product> quickSort(ArrayList<Product> items){
+		return items;
+	}
+}
