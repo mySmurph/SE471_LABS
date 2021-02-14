28105193040fbd9fcf16968b49fdbf5a03a549ad
laurengonz <laurengonz@users.noreply.github.com>
2021-02-14 12:42:30 -0800

    product uml

--- SortingUtility/src/Product.java
+++ SortingUtility/src/Product.java
@@ -1,8 +1,72 @@
 
 public class Product {
-
-	public Product() {
+	
+	/**
+	 * general product ID 
+	 */
+	private String productID;
+	
+	
+	/**
+	 * general product price 
+	 */
+	private String productName;
+	
+	/**
+	 * 
+	 */
+	private float productPrice;
+	
+	
+	/**
+	 * constructor
+	 * @param productID		the product ID
+	 * @param productName	the product name
+	 * @param productPrice	the product price
+	 */
+	public Product(String productID, String productName, float productPrice) {
 		// TODO Auto-generated constructor stub
+		this.productID = productID;
+		this.productName = productName;
+		this.productPrice = productPrice;
+	}
+	
+	
+	/**
+	 * @return the productID
+	 */
+	public String getproductID(){
+		return productID;
+	}
+	
+	/**
+	 * @return the product name
+	 */
+	public String getproductName() {
+		return productName;
+	}
+	
+	/**
+	 * @return the product price
+	 */
+	public float getproductPrice() {
+		return productPrice;
 	}
 
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setproductName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @param price the price to set
+	 */
+	
+	public void setProductPrice(float price) {
+		this.productPrice = price;
+	}
+	
 }
