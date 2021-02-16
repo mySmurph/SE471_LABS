a07bf94f94363a6afe1047c722713edd12a362c3
mySmurph <56486787+mySmurph@users.noreply.github.com>
2021-02-16 08:09:27 -0800

    Just need comments now
    :D

--- 2_SortingUtility/src/SortingUtilityLog.java
+++ 2_SortingUtility/src/SortingUtilityLog.java
@@ -1,33 +1,38 @@
 package src;
 
-import java.lang.reflect.*;
-//import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
 
 public class SortingUtilityLog implements ISortingUtility {
 	
 	private SortingUtility sortingUtil_original;
-	private Class<? extends SortingUtility> suClass;
-	private Method sortMethod;
+//	private Method sortMethod;
 
 	public SortingUtilityLog(SortingUtility su) {
 		this.sortingUtil_original = su;
 	}
 	
-	private void reflection(List<Product> items, int sortingApproach) throws NoSuchMethodException, InvocationTargetException{
-		suClass = sortingUtil_original.getClass();
-//		sortMethod = suClass.getMethod("sort", List.class, int.class);
-		sortMethod = suClass.getDeclaredMethod("foo", int.class, int.class);
+	private Method reflectionSortMethod(String method) throws NoSuchMethodException, InvocationTargetException{
+		Method sortMethod = sortingUtil_original.getClass().getDeclaredMethod(method, List.class);
 		sortMethod.setAccessible(true);
-		System.out.println(sortMethod.toString());
+		return sortMethod;
+//		System.out.println(sortMethod.toString());
 	}
 	
 	public List<Product> sort(List<Product> items, int sortingApproach){
 		try {
-			this.reflection(items, sortingApproach);
-//			items = (List<Product>) sortMethod.invoke(sortingUtil_original, items, sortingApproach);	
-			sortMethod.invoke(sortingUtil_original,1, 2);
+
+			switch(sortingApproach) {
+				case 1:		items = (List<Product>) (this.reflectionSortMethod("bubbleSort")).invoke(sortingUtil_original, items);	
+							bubbleSortPrint(items);
+							break;
+				case 2:		items = (List<Product>) (this.reflectionSortMethod("quickSort")).invoke(sortingUtil_original, items);
+							quickSortPrint(items);
+							break;
+				default:	break;
+			}
 		}catch (NoSuchMethodException e) {
 			System.out.println("No Such Method");
 			e.printStackTrace();
@@ -36,49 +41,6 @@ public class SortingUtilityLog implements ISortingUtility {
 		} catch(IllegalAccessException e) {}
 		return items;
 	}
-	/*
-	private List<Product> bubbleSort(List<Product>  items){
-		int max = items.size();
-		for(int i = 0; i < max-1; i++) {
-			for(int ii = i+1; ii < max ; ii++) {
-				if(items.get(i).getProductID() > items.get(ii).getProductID()) {
-					Collections.swap(items, i, ii);
-				}
-			}
-		}
-		bubbleSortPrint(items);
-		return items;
-	}
-	*/
-	private List<Product> quickSort(List<Product>  items){
-		quickSort(items, 0, items.size()-1);
-		quickSortPrint(items);
-		return items;
-	}
-	
-	private void quickSort(List<Product>  items, int first, int last) {
-		int i = first;
-		int ii = last;
-		int pivotID = items.get((first + last)/2).getProductID();
-		do {
-			while(items.get(i).getProductID() < pivotID)
-				i++;
-			while(items.get(ii).getProductID() > pivotID)
-				ii--;
-			if(i <= ii) {
-				Collections.swap(items, i, ii);
-				i++;
-				ii--;
-			}
-			
-		}while(i <= ii);
-		
-		if(first < ii)
-			quickSort(items, first, ii);
-		if(i < last)
-			quickSort(items, i, last);
-	}
-	
 	
 	private void bubbleSortPrint(List<Product> items) {
 		System.out.println("\nBubble Sort Called");
